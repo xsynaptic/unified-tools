3c73279a865aa02602edadf8865c663a083b70cb
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2025-06-11 08:40:28 +0800

    chore: update dependencies and bump version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check -- ESLint still lacks support for config files in native TypeScript
-import eslint from "@eslint/js";
-import simpleImportSortPlugin from "eslint-plugin-simple-import-sort";
-import unicornPlugin from "eslint-plugin-unicorn";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
+import unicornPlugin from 'eslint-plugin-unicorn';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 const config = tseslint.config(
   {
-    ignores: ["**/node_modules", "**/dist", "dist/**/*", "**/node_modules"],
+    ignores: ['**/node_modules', '**/dist', 'dist/**/*', '**/node_modules'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
@@ -17,7 +17,7 @@ const config = tseslint.config(
       parser: tseslint.parser,
       parserOptions: {
         projectService: {
-          allowDefaultProject: ["*.mjs", "tests/*.test.ts"],
+          allowDefaultProject: ['*.mjs', 'tests/*.test.ts'],
         },
       },
       globals: {
@@ -26,16 +26,16 @@ const config = tseslint.config(
       },
     },
     plugins: {
-      "@typescript-eslint": tseslint.plugin,
+      '@typescript-eslint': tseslint.plugin,
     },
   },
   {
     plugins: {
-      "simple-import-sort": simpleImportSortPlugin,
+      'simple-import-sort': simpleImportSortPlugin,
     },
     rules: {
-      "simple-import-sort/imports": [
-        "warn",
+      'simple-import-sort/imports': [
+        'warn',
         {
           groups: [
             [String.raw`^@?\w`], // External packages
@@ -50,20 +50,20 @@ const config = tseslint.config(
           ],
         },
       ],
-      "simple-import-sort/exports": "warn",
+      'simple-import-sort/exports': 'warn',
     },
   },
-  unicornPlugin.configs["flat/recommended"],
+  unicornPlugin.configs.recommended,
   {
     rules: {
-      "unicorn/filename-case": [
-        "warn",
+      'unicorn/filename-case': [
+        'warn',
         {
-          case: "kebabCase",
+          case: 'kebabCase',
           ignore: [String.raw`^(README|TODO)\.md$`],
         },
       ],
-      "unicorn/prevent-abbreviations": "off",
+      'unicorn/prevent-abbreviations': 'off',
     },
   }
 );
